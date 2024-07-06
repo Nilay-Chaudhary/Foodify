fea176c9d959513cc6da4500425feff0081fc971
Nilay-Chaudhary <nilaychaudhary04@gmail.com>
2024-07-07 02:11:41 +0530

    test

--- backend/Routes/displayData.js
+++ backend/Routes/displayData.js
@@ -4,7 +4,6 @@ const router = express.Router();
 router.post("/fooddata", (req, res) => {
     try {
         if(global.food_items != null && global.foodCategory != null) res.send([global.food_items, global.foodCategory])
-        else res.send([]);
     } catch (error) {
         console.error("Error fetching data: " + error);
         res.status(500).send("Server error")
